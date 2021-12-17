e11a9a616ffe0140be69c529ca2511f89cd5babb
Orcohen33 <92351152+Orcohen33@users.noreply.github.com>
2021-12-17 19:23:08 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 int main() {
     char word[30];
     char text[1024];
+    for (int i = 0; i < 30; ++i) {word[i] = '\000';}
+    for (int i = 0; i < 1024; ++i) {text[i] = '\000';}
     scanf("%[^\n]", word);
     scanf("%[^~]", text);
     int wordlen = strlen(word);
     int textlen = strlen(text);
     char *w = word;
     char *t = text;
-
     printf("Gematria Sequences: ");
     q1(w,t,wordlen,textlen);
     printf("\n");
