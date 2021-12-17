eab699fe8988bb4524857d2fdbfac2e4b7669d90
Orcohen33 <92351152+Orcohen33@users.noreply.github.com>
2021-12-18 01:44:16 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,14 +4,13 @@
 int main() {
     char word[30];
     char text[1024];
-    for (int i = 0; i < 30; ++i) {word[i] = '\000';}
-    for (int i = 0; i < 1024; ++i) {text[i] = '\000';}
     scanf("%[^\n]", word);
     scanf("%[^~]", text);
     int wordlen = strlen(word);
     int textlen = strlen(text);
     char *w = word;
     char *t = text;
+
     printf("Gematria Sequences: ");
     q1(w,t,wordlen,textlen);
     printf("\n");
@@ -20,6 +19,5 @@ int main() {
     printf("\n");
     printf("Anagram Sequences: ");
     q3(w,t,wordlen,textlen);
-    printf("\n");
     return 0;
 }
